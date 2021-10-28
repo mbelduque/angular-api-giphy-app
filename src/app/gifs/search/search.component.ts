a3a757a880e4612bc2657a165da2883b2081e870
Mauricio Belduque Guzman <mbelduqu@bancolombia.com.co>
2021-10-28 00:04:16 -0500

    add search component

---
+++ src/app/gifs/search/search.component.ts
@@ -0,0 +1,26 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+import {GifsService} from "../services/gifs.service";
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifService: GifsService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  search() {
+    const value = this.txtSearch.nativeElement.value;
+    this.gifService.searchGifs(value);
+    this.txtSearch.nativeElement.value = '';
+  }
+
+}
