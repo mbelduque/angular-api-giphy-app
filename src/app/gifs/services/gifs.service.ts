6c0435c12e4af523a2c9cac3396c410f09d16652
Mauricio Belduque Guzman <mbelduqu@bancolombia.com.co>
2021-12-08 22:58:22 -0500

    add http params

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Gif, SearchGifsResponse} from "../interfaces/interfaces-gifs";
 
 @Injectable({
@@ -7,14 +7,15 @@ import {Gif, SearchGifsResponse} from "../interfaces/interfaces-gifs";
 })
 export class GifsService {
 
-  private API_KEY = 'mTe1eTIkGffodzp5kVF7HrUAQAhoPOp6';
-  private BASE_URL = 'https://api.giphy.com/v1/gifs/'
+  private API_KEY: string = 'mTe1eTIkGffodzp5kVF7HrUAQAhoPOp6';
+  private BASE_URL: string = 'https://api.giphy.com/v1/gifs';
+  private LIMIT: string = '10';
 
   private _record: string[] = [];
   public results: Gif[] = [];
 
   constructor(private http: HttpClient) {
-    if (localStorage.getItem('historial') && localStorage.getItem('resultados') ) {
+    if (localStorage.getItem('historial') && localStorage.getItem('resultados')) {
       this._record = JSON.parse(localStorage.getItem('historial')!)
       this.results = JSON.parse(localStorage.getItem('resultados')!)
     }
@@ -34,11 +35,14 @@ export class GifsService {
       this._record = this._record.splice(0, 10);
       localStorage.setItem('historial', JSON.stringify(this._record));
     }
-    this.http.get<SearchGifsResponse>(`${this.BASE_URL}search?api_key=${this.API_KEY}&q=${query}&limit=10`)
+    const httpParams = new HttpParams()
+      .set('api_key', this.API_KEY)
+      .set('limit', this.LIMIT)
+      .set('q', query);
+    this.http.get<SearchGifsResponse>(`${this.BASE_URL}/search`, {params: httpParams})
       .subscribe((response) => {
         this.results = response.data;
         localStorage.setItem('resultados', JSON.stringify(this.results));
-        console.log(response.data);
       });
   }
 
